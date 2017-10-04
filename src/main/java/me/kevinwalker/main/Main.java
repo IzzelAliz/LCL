a2a2612dca11bb0f4212e53f3ae1c1be41a21ff7
Izzel_Aliz <csh2001331@126.com>
2017-10-04 13:55:41 +0800

    .

---
+++ src/main/java/me/kevinwalker/main/Main.java
@@ -0,0 +1,62 @@
+package me.kevinwalker.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/LoginCraftLaunch.fxml"));
+        root.setOnMousePressed((MouseEvent event) -> {
+            event.consume();
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+        root.setOnMouseDragged((MouseEvent event) -> {
+            event.consume();
+            primaryStage.setX(event.getScreenX() - xOffset);
+
+            if (event.getScreenY() - yOffset < 0) {
+                primaryStage.setY(0);
+            } else {
+                primaryStage.setY(event.getScreenY() - yOffset);
+            }
+        });
+        primaryStage.setTitle("LoginCraftLaunch-0.0.1Demo");
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setResizable(false);
+        Scene scene = new Scene(root,800,500);
+        scene.setFill(null);
+//        scene.getStylesheets().add(getClass().getResource("/css/MainUI.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+//        ServerListPing17 slp = new ServerListPing17();
+//        InetSocketAddress sadd0 = new InetSocketAddress("dx.mc11.icraft.cc", 37190);
+//        InetSocketAddress sadd1 = new InetSocketAddress("218.93.208.142", 10648);
+//        InetSocketAddress sadd2 = new InetSocketAddress("four.mengcraft.com", 13433);
+//        InetSocketAddress sadd3 = new InetSocketAddress("r2.suteidc.com", 26339);
+//        InetSocketAddress sadd = new InetSocketAddress("103.37.45.108", 7160);
+//
+//        slp.setAddress(sadd);
+//        ServerListPing17.StatusResponse sr = slp.fetchData();
+//        System.out.println("最大在线" + sr.getPlayers().getMax());
+//        System.out.println("在线" + sr.getPlayers().getOnline());
+//        if(sr.getPlayers().getSample()!= null) {
+//            for (int i = 0; i < sr.getPlayers().getSample().size(); i++) {
+//                System.out.println("玩家"+i+":"+sr.getPlayers().getSample().get(i).getName());
+//            }
+//        }
+    }
+}
